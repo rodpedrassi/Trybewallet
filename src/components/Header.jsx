c3cde8bdd83445ebdaf0b4e21c60a105130affa3
rodpedrassi <rodpedrassi@gmail.com>
2022-08-26 19:14:55 -0300

    requisito 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../css/header.css';
+
+class Header extends Component {
+  state = {
+    total: 0,
+    currency: 'BRL',
+  };
+
+  render() {
+    const { email } = this.props;
+    const { total, currency } = this.state;
+    return (
+      <div>
+        <header className="header-box">
+          <div>
+            <p data-testid="email-field">{`Usuário: ${email}`}</p>
+          </div>
+          <div>
+            <span data-testid="total-field">{`Total: ${total}`}</span>
+            <span data-testid="header-currency-field">{currency}</span>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends Component {
   render() {
-    console.log(this.props);
-    const { email, password } = this.props;
-
     return (
       <div>
-        <p>{`Usuario: ${email}`}</p>
-        <p>{`Senha? ${password}`}</p>
+        <Header />
       </div>
     );
   }
 }
-Wallet.propTypes = {
-  email: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
-};
 
-const mapStateToProps = (state) => ({
-  email: state.user.email,
-  password: state.user.password,
-});
-
-export default connect(mapStateToProps)(Wallet);
+export default connect()(Wallet);
